f2ef73e1268ac43807d24042675d57125fc6109f
subas-roy <subasroy46@gmail.com>
2025-03-10 17:49:40 +0600

    Install and configure shadcn/ui

--- steps.js
+++ steps.js
@@ -1,4 +1,6 @@
 /**
  * 1. npm create vite@latest task-management-with-redux -- --template react-ts
  * 2. npm install @reduxjs/toolkit react-redux
+ *    https://ui.shadcn.com/docs/installation/vite
+ * 3. npm install tailwindcss @tailwindcss/vite
  */
