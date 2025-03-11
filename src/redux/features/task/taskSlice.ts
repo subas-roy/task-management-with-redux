1f646dbd73ca5c7c2ea653ce486ffe37df211fb5
subas-roy <subasroy46@gmail.com>
2025-03-11 09:49:08 +0600

    Initiate task slice and initial state. Export reducer.

---
+++ src/redux/features/task/taskSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Schema
+const initialState = {
+  task: {
+    id: 'asdfjklmn',
+    title: 'Initial frontend',
+    description: 'Create Home page and routing',
+    dueDate: '2025-3',
+    isCompleted: false,
+    priority: 'High',
+  },
+};
+
+const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {},
+});
+
+export default taskSlice.reducer;
