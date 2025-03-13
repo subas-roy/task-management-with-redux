636920110087558ed82ab0f0cac9cc69ecfa5312
subas-roy <subasroy46@gmail.com>
2025-03-13 18:32:40 +0600

    Console log tasks

--- src/components/pages/tasks.tsx
+++ src/components/pages/tasks.tsx
@@ -1,4 +1,10 @@
+import { useAppSelector } from '@/redux/hook';
+
 const Tasks = () => {
+  const tasks = useAppSelector((state) => state.todo.tasks);
+
+  console.log(tasks);
+
   return (
     <div>
       <h1>This is tasks component</h1>
