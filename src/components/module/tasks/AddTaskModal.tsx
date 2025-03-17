f4a262822bb42953b7185c4e16ff35dda6ad7a4b
subas-roy <subasroy46@gmail.com>
2025-03-17 20:05:24 +0600

    Added onSubmit type SubmitHandler<FieldValues> and data as ITask assertion

--- src/components/module/tasks/AddTaskModal.tsx
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -34,17 +34,19 @@ import { Textarea } from '@/components/ui/textarea';
 import { cn } from '@/lib/utils';
 import { addTask } from '@/redux/features/task/taskSlice';
 import { useAppDispatch } from '@/redux/hook';
+import { ITask } from '@/types';
 import { Description } from '@radix-ui/react-dialog';
 import { format } from 'date-fns';
 import { CalendarIcon } from 'lucide-react';
-import { useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 const AddTaskModal = () => {
   const form = useForm();
 
   const dispatch = useAppDispatch();
 
-  const onSubmit = (data) => {
-    dispatch(addTask(data));
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    // type assertion
+    dispatch(addTask(data as ITask));
   };
 
   return (
