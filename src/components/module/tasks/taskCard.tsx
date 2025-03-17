040df8f031293546bfd943de7bd54086b4bc9c49
subas-roy <subasroy46@gmail.com>
2025-03-18 05:50:15 +0600

    Implement toggleCompleteState in component level TaskCard

--- src/components/module/tasks/taskCard.tsx
+++ src/components/module/tasks/taskCard.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { cn } from '@/lib/utils';
+import { toggleCompleteState } from '@/redux/features/task/taskSlice';
+import { useAppDispatch } from '@/redux/hook';
 import { ITask } from '@/types';
 import { Trash } from 'lucide-react';
 
@@ -9,6 +11,7 @@ interface IProps {
 }
 
 const TaskCard = ({ task }: IProps) => {
+  const dispatch = useAppDispatch();
   return (
     <div className="border px-5 py-3 rounded-md">
       <div className="flex justify-between items-center">
@@ -26,7 +29,7 @@ const TaskCard = ({ task }: IProps) => {
           <Button variant="link" className="p-0 text-red-500">
             <Trash />
           </Button>
-          <Checkbox />
+          <Checkbox onClick={() => dispatch(toggleCompleteState(task.id))} />
         </div>
       </div>
       <p className="mt-5">{task.description}</p>
