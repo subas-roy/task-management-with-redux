4223508556022ebd7dc7bad4663f62c4ba3cf1bf
subas-roy <subasroy46@gmail.com>
2025-03-20 09:30:17 +0600

    Display users

--- src/components/pages/users.tsx
+++ src/components/pages/users.tsx
@@ -1,7 +1,33 @@
+import { AddUserModal } from '@/components/module/users/AddUserModal';
+import { removeUser, selectUsers } from '@/redux/features/user/userSlice';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import { Trash2 } from 'lucide-react';
+
 const Users = () => {
+  const dispatch = useAppDispatch();
+  const users = useAppSelector(selectUsers);
+
   return (
-    <div>
-      <h1>This is users component</h1>
+    <div className="mx-auto max-w-7xl px-5 mt-20">
+      <div className="flex justify-end items-center gap-5">
+        <h1 className="mr-auto">Users</h1>
+        <AddUserModal />
+      </div>
+      <div className="grid grid-cols-3 gap-5 mt-3">
+        {users.map((user) => (
+          <div className="p-10 flex justify-between border border-green-500">
+            <div>
+              <p>{user.name}</p>
+            </div>
+            <div>
+              <Trash2
+                onClick={() => dispatch(removeUser(user.id))}
+                className="text-red-500"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
